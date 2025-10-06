3855be881c68739e4d438375d864b76c6ddbb9e6
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-10-07 02:34:47 +0500

    modify js

--- main.js
+++ main.js
@@ -3,54 +3,58 @@ let addInput = document.querySelector(".addInput");
 let addBtn = document.querySelector(".addBtn");
 
 let lists = [
-  //   { id: 123, text: "sam" },
-  //   { id: 321, text: "uffexx" },
-];
+  // {id : 23, text : "sam"},
+  // {id : 23, text : "uffexx"},
+]
 
-function renderList(arr) {
-  listContainer.innerHTML = "";
-  arr.forEach((obj) => {
-    // console.log(list);
+function render(arr){
+  listContainer.innerHTML = ""
+  arr.forEach((obj)=>{
     let divElem = document.createElement("div");
-    divElem.classList.add("list");
-    divElem.innerHTML = `
-     <div class="list">
-                <p>${obj.text}</p>
-     </div>
-    `;
-    let btnElem = document.createElement("button");
-    btnElem.textContent = "❌";
-    divElem.append(btnElem);
-    listContainer.append(divElem);
+    divElem.className = "list";
+    let para = document.createElement("p");
+    para.textContent = obj.text;
+    let btn = document.createElement("button");
+    btn.textContent = "❌";
+    divElem.append(para, btn);
+    btn.dataset.id = obj.id;
 
-    btnElem.onclick = ()=>{
-        handleRemoveBtn(obj.id)
-    }
-  });
-  addInput.value = "";
+    listContainer.append(divElem);
+    
+  })
+}
+render(lists)
+
+
+function addHandleBtn(){
+  let text = addInput.value.trim();
+  if (!text) return;
+  let obj ={
+    text : text,
+    id : Date.now(),
+  }
+  lists.push(obj)
+
+  render(lists);
+  addInput.value = ""
   
 }
-renderList(lists);
 
-function handleAddBtn() {
-    if (!addInput.value.trim()) return
-  let obj = {
-    id: Date.now(),
-    text: addInput.value,
-  };
-  lists.push(obj);
-  renderList(lists);
+addBtn.addEventListener("click", addHandleBtn);
+addInput.addEventListener("keypress", (e)=>{
+  if (e.key === "Enter") addHandleBtn()
+})
+
+
+function handleDeleteBtn(id){
+  lists = lists.filter((obj)=> obj.id !== id);
+  render(lists)
+  
 }
+listContainer.addEventListener("click", (e)=>{
+  if (e.target.tagName === "BUTTON"){
+    let id = Number(e.target.dataset.id);
+    handleDeleteBtn(id)
+  }
+})
 
-function handleRemoveBtn(id){
-    let filteredList = lists.filter((obj)=>{
-        return obj.id !== id;
-    })
-    lists = filteredList;
-    renderList(filteredList)
-}
-addBtn.addEventListener("click", handleAddBtn);
-
-addInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") handleAddBtn();
-});
